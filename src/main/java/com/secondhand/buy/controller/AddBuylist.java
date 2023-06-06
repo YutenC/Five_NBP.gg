2d5df0506105b9a1a68f8567d60b6930125e10e7
NewNeko <appay9563@gmail.com>
2023-06-06 21:09:11 +0800

    測試insert資料

--- src/main/java/com/secondhand/buy/controller/AddBuylist.java
+++ src/main/java/com/secondhand/buy/controller/AddBuylist.java
@@ -3,6 +3,7 @@ package com.secondhand.buy.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +12,16 @@ import com.secondhand.buy.dao.impl.SecondHandBuylistDaoimpl;
 import  com.secondhand.buy.util.Json2vo;
 import com.secondhand.buy.vo.SecondHandBuylist;
 
+
+@WebServlet("/secondhand/addbuylist")
 public class AddBuylist extends HttpServlet  {
 	private static final long serialVersionUID = -4669764916210514485L;
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8"); 
 		SecondHandBuylist buylist = Json2vo.jsonTovo(req, SecondHandBuylist.class);
+		
 		if (buylist == null) {
 			buylist = new SecondHandBuylist();
 			buylist.setProductName("無商品名稱");
