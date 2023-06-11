da898e262067d5c942519379a191e440e652f8b1
Rae Wang <wyt.raewang@gmail.com>
2023-06-11 15:25:49 +0800

    更新二手商品TestApp

--- src/main/java/com/secondhand/sale/test/TestApp.java
+++ src/main/java/com/secondhand/sale/test/TestApp.java
@@ -1,28 +1,100 @@
 package com.secondhand.sale.test;
 
 import com.core.util.HibernateUtil;
+import com.secondhand.sale.dao.SecondhandProductDao;
+import com.secondhand.sale.dao.impl.SecondhandProductDaoImpl;
 import com.secondhand.sale.entity.SecondhandProduct;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.Date;
+import java.util.List;
 
 public class TestApp {
 
     public static void main(String[] args) {
-        SecondhandProduct sp = new SecondhandProduct();
-        sp.setName("PS手把");
-        sp.setType("01");
-        sp.setPrice(2000);
-        sp.setContent("abcdefghijklmn");
-        sp.setLaunchTime(new Date());
+//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//        Session session = sessionFactory.openSession();
+
+// ===========新增
+//        SecondhandProduct sp = new SecondhandProduct();
+//        sp.setName("XBOX主機");
+//        sp.setType("04");
+//        sp.setPrice(200);
+//        sp.setContent("aaaaaa");
+//        sp.setLaunchTime(new Date());
+//
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        spdao.insert(sp);
 
 
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
+// ===========刪除
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        spdao.deleteById(4);
 
-        SecondhandProduct secondhandProduct = session.get(SecondhandProduct.class, 1);
-        System.out.println(secondhandProduct.getContent());
+
+// ===========修改
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        SecondhandProduct newsp = spdao.getSession().get(SecondhandProduct.class, 2);
+//        newsp.setContent("TESTnewcontentttttt");
+//        newsp.setLaunchTime(new Date());
+//        spdao.update(newsp);
+
+// ===========selectById
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        SecondhandProduct spinfo = spdao.selectById(2);
+//        System.out.println(spinfo.getName());
+
+
+// ===========selectAll
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        List<SecondhandProduct> allsp = spdao.selectAll();
+//        if (!(allsp.isEmpty())) {
+//            for (SecondhandProduct sp : allsp) {
+//                System.out.println(sp.getProductId());
+//                System.out.println(sp.getName());
+//            }
+//        }else{
+//            System.out.println("未找到任何商品");
+//        }
+
+// ===========selectByTime
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+//        List<SecondhandProduct> spLaunchTime = spdao.selectByTime();
+//        if (!(spLaunchTime.isEmpty())) {
+//            for (SecondhandProduct sp : spLaunchTime) {
+//                System.out.println(sp.getProductId());
+//                System.out.println(sp.getName());
+//                System.out.println(sp.getLaunchTime());
+//            }
+//        }else{
+//            System.out.println("fail");
+//        }
+
+
+
+        // ===========其它測試
+//        Transaction trans = session.beginTransaction();
+//        trans.commit();
+
+//        System.out.println(spdao.selectAll());
+
+//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//        Session session = sessionFactory.openSession();
+//        SecondhandProductDao spdao = new SecondhandProductDaoImpl();
+        //        Transaction trans = session.beginTransaction();
+//        spdao.getSession();
+//        spdao.insert(sp);
+
+//        System.out.println(spdao.selectAll());
+
+//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//        Session session = sessionFactory.openSession();
+//        Transaction trans = session.beginTransaction();
+//        session.persist(sp);
+//        trans.commit();
+//        System.out.println(session.get(SecondhandProduct.class, 2).getContent());
     }
 
 
