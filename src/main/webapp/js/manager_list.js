26dad61f64a39734317a2e370f8813b4fbfe5668
WuSeagal <wuseagal@gmail.com>
2023-06-12 19:16:10 +0800

    更新 manager_list前端JS搜尋功能 ｜ manager_list 搜尋完結果重新呈現畫面& 清除搜尋條件，現在會在在讀取入畫面、搜尋、清除搜尋的時候重新印出表格

--- src/main/webapp/js/manager_list.js
+++ src/main/webapp/js/manager_list.js
@@ -1,4 +1,7 @@
 let manager_array = [];
+let filtered_array = [];
+let filtered = false;
+let managerListContainer = document.querySelector('table#manager_list tbody');
 
 // 使用 AJAX 發送請求獲取 managerList 的資料
 
@@ -8,11 +11,11 @@ fetch('../manager/manager_list', {
     .then(response => response.json())
     .then(data => {
         // 獲取到 managerList 的資料後，動態生成 HTML 內容
-        let managerListContainer = document.querySelector('table#manager_list tbody');
+
 
         // console.log(managerListContainer);
 
-        let html = '';
+
 
 
         // console.log(data.managerList);
@@ -35,57 +38,62 @@ fetch('../manager/manager_list', {
             console.log(manager_array_item);
             // console.log(manager_array);
 
+            showList();
+        })
 
 
-            html += `
-        <tr>
-          <td>${manager.manager_id}</td>
-          <td>${manager.account}</td>
-          <td class="manager-password">${manager.password}</td>
-          <td>${manager.name}</td>
-          <td>${manager.email}</td>
-          <td> ${manager.phone}</td>
-          <td>${manager.is_working}</td>
-          <td>
-              <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
-                  role="button" href="#"
-                  onclick="ShowAllInfoClick(${manager.manager_id})">
-                  詳細資料
-              </a>
-              <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
-                  role="button" href="#"
-                  onclick="ChangeStateClick(${manager.manager_id})">
-                  調整在職狀態
-              </a>
-              <br style="padding-top: 3px; padding-buttom: 3px;">
-              <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
-                  role="button" href="manager_edit.html"
-                  onclick="EditClick(${manager.manager_id})">
-                  修改資料
-              </a>
-              <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button btn-danger"
-                  role="button" href="#"
-                  onclick="RemoveClick(${manager.manager_id})">
-                  刪除管理員
-              </a>
-          </td>
-        </tr>
-      `;
-        });
-
-        // 插入生成的 HTML 內容到 managerListContainer 元素中
-        managerListContainer.innerHTML = html;
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
+$("a.manager_search_button").on("click", () => {
+    event.preventDefault();
+    filtered = true;
+
+    let searchType = $("select.manager_search_type").val();
+    let searchContent = $("input.manager_search_content").val();
+
+    alert(typeof searchType + searchType + "\n" + typeof searchContent + searchContent);
+
+
+    filtered_array = manager_array.filter((manager) => {
+        if (typeof manager[searchType] === "number") {
+            let searchContent_num = /^[0-9]+$/.test(searchContent) ? parseInt(searchContent) : NaN;
+            if (searchContent_num === NaN) {
+                alert("該欄只能搜尋數字");
+            } else {
+
+                console.log(typeof manager[searchType]);
+                console.log(typeof searchContent);
+                return manager[searchType].toString().includes(searchContent);
+            }
+        }
+
+        return manager[searchType].includes(searchContent);
+
+
+    });
+
+    console.log(filtered_array);
+    showList();
+
+})
+
+$("a.manager_default_list_button").on("click", () => {
+    filtered = false;
+    showList();
+})
+
+
+
 
 function ShowAllInfoClick(id) {
     console.log(id);
     alert(manager_array[id].address);
 }
 
-//WIP
+
 function ChangeStateClick(id) {
     event.preventDefault();
 
@@ -118,7 +126,6 @@ function ChangeStateClick(id) {
 }
 
 function EditClick(id) {
-
     // 將 ID 儲存到 sessionStorage 中
     sessionStorage.setItem('id', id);
 
@@ -169,5 +176,59 @@ function RemoveClick(id) {
     } else {
         console.log("R U joking?");
     }
+
+
+
+}
+
+function showList() {
+    let showArray = manager_array;
+    if (filtered) {
+        showArray = filtered_array;
+    }
+
+    let html = '';
+
+    showArray.forEach(manager => {
+        html += `
+    <tr>
+      <td>${manager.manager_id}</td>
+      <td>${manager.account}</td>
+      <td class="manager-password">${manager.password}</td>
+      <td>${manager.name}</td>
+      <td>${manager.email}</td>
+      <td> ${manager.phone}</td>
+      <td>${manager.is_working}</td>
+      <td>
+          <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
+              role="button" href="#"
+              onclick="ShowAllInfoClick(${manager.manager_id})">
+              詳細資料
+          </a>
+          <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
+              role="button" href="#"
+              onclick="ChangeStateClick(${manager.manager_id})">
+              調整在職狀態
+          </a>
+          <br style="padding-top: 3px; padding-buttom: 3px;">
+          <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button"
+              role="button" href="manager_edit.html"
+              onclick="EditClick(${manager.manager_id})">
+              修改資料
+          </a>
+          <a class="btn btn-primary btn-sm d-none d-sm-inline-block custom-manager-button btn-danger"
+              role="button" href="#"
+              onclick="RemoveClick(${manager.manager_id})">
+              刪除管理員
+          </a>
+      </td>
+    </tr>
+  `;
+    });
+
+
+
+    // 插入生成的 HTML 內容到 managerListContainer 元素中
+    managerListContainer.innerHTML = html;
 
 }
